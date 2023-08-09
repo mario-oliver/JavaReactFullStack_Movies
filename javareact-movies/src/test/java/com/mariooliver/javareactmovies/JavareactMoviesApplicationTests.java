83490d5d23c0fb88452064cc5fd34ef5ef54018c
Mario Oliver <mar.mariooliver93@gmail.com>
2023-08-09 18:05:37 -0400

    Created Spring boot project with java 20 and the following dependencies: Spring Boot DevTools, Lombock, Spring Web, and Spring Data MongoDB

---
+++ javareact-movies/src/test/java/com/mariooliver/javareactmovies/JavareactMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariooliver.javareactmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavareactMoviesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
